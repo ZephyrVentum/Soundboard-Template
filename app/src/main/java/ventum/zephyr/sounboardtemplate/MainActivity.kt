cd23266dcd7e986e6775140270f8c25af36960c3
Alexander Klimenko <>
2018-08-03 01:32:31 +0300

    refactored ripple effect, refactored code, added recycle view dependency

--- app/src/main/java/ventum/zephyr/sounboardtemplate/MainActivity.kt
+++ app/src/main/java/ventum/zephyr/sounboardtemplate/MainActivity.kt
@@ -1,14 +1,18 @@
 package ventum.zephyr.sounboardtemplate
 
+import android.content.res.ColorStateList
 import android.databinding.DataBindingUtil
 import android.graphics.PorterDuff
+import android.graphics.drawable.RippleDrawable
 import android.os.Bundle
+import android.support.annotation.DrawableRes
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import ventum.zephyr.sounboardtemplate.databinding.ActivityMainBinding
-import com.bumptech.glide.request.RequestOptions.bitmapTransform
+import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions.bitmapTransform
 import jp.wasabeef.glide.transformations.BlurTransformation
-
+import ventum.zephyr.sounboardtemplate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +21,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.testImage.setColorFilter(R.color.item_image_mask_color, PorterDuff.Mode.SRC_OVER)//DST_OUT
         Glide.with(this).load(R.drawable.bg_main)
                 .apply(bitmapTransform(BlurTransformation(22)))
                 .into(binding.bgImageView)
+
+        setupImage(binding.testImage, R.drawable.img_victory)
+        setupImage(binding.test2Image, R.drawable.img_defend)
+        setupImage(binding.test3Image, R.drawable.blind)
     }
+
+    private fun setupImage(view: ImageView, @DrawableRes drawable: Int){
+        view.setColorFilter(ContextCompat.getColor(this, R.color.item_image_mask_color), PorterDuff.Mode.SRC_OVER)
+        view.setImageDrawable(getRippleDrawable(drawable))
+    }
+
+    private fun getRippleColorStateList(color: Int): ColorStateList = ColorStateList(arrayOf(intArrayOf()), intArrayOf(color))
+
+    private fun getRippleDrawable(@DrawableRes drawable: Int): RippleDrawable = RippleDrawable(
+            getRippleColorStateList(ContextCompat.getColor(this, R.color.ripple_effect_color)),
+            getDrawable(drawable), null)
 }
---
+++ app/src/main/java/ventum/zephyr/sounboardtemplate/SoundsAdapter.kt
@@ -0,0 +1,19 @@
+package ventum.zephyr.sounboardtemplate
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class SoundsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
