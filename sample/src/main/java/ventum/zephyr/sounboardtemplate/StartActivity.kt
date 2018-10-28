d70b97a8796622f8903e25219a33bfdf0d28c039
Alexander Klimenko <ikytosan0@gmail.com>
2018-10-28 21:18:50 +0200

    removed redundant files

--- sample/src/main/java/ventum/zephyr/sounboardtemplate/StartActivity.kt
+++ sample/src/main/java/ventum/zephyr/sounboardtemplate/StartActivity.kt
@@ -16,21 +16,21 @@ class StartActivity : SoundboardActivity() {
     private fun createFirstCategory(): SoundboardCategory {
         val soundItems = SoundItems()
         for (i in 0..6 step 1) {
-            soundItems.add(SoundItem(R.drawable.bg_main, R.raw.lvlup_sound, soundPool.load(this, R.raw.lvlup_sound, 1)))
+            soundItems.add(SoundItem(R.drawable.bg_main, R.raw.example_sound, soundPool.load(this, R.raw.example_sound, 1)))
         }
         return SoundboardCategory("First", soundItems)
     }
 
     private fun createSecondCategory() = SoundboardCategory("Second", SoundItems().apply {
         for (i in 0..11) {
-            add(SoundItem(R.drawable.bg_main, R.raw.lvlup_sound, soundPool.load(this@StartActivity, R.raw.lvlup_sound, 1)))
+            add(SoundItem(R.drawable.bg_main, R.raw.example_sound, soundPool.load(this@StartActivity, R.raw.example_sound, 1)))
         }
     })
 
     private fun createThirdCategory(): SoundboardCategory {
         val soundItems = SoundItems()
         for (i in 0..5) {
-            soundItems.add(SoundItem(R.drawable.bg_main, R.raw.lvlup_sound, R.string.example_sound_name, soundPool.load(this, R.raw.lvlup_sound, 1)))
+            soundItems.add(SoundItem(R.drawable.bg_main, R.raw.example_sound, R.string.example_sound_name, soundPool.load(this, R.raw.example_sound, 1)))
         }
         return SoundboardCategory("Third", soundItems)
     }
